41b2f8d3f11e1f8cb06c756a3e166fd2a638d50a
rschuessler <rschuessler@csc.com>
2018-07-27 01:12:34 +0200

    Post interface

---
+++ src/models/post.interface.ts
@@ -0,0 +1,34 @@
+
+/**
+ * An Interface for the Post Object
+ */
+export interface Post{
+
+    userId: string;
+    title: string;
+    content: string;
+    // type of Media
+    type: string;
+    fileName: string;
+    // link to multimedia
+    link: string;
+    created: Date;
+    modified: Date;
+    country: string;
+    city: string;
+    //Neighbourhood
+    vicinity: string;
+    org: string;
+    badCount: number;
+    goodCount: number;
+    views: number;
+    tags: string;
+    language: string;
+    commentsCount: number;
+    sharesCount: number;
+    trashCount: number;
+    usersIncluded: number;
+    chatDuration: number;
+    postOpened: number;
+    mood: number;
+}
